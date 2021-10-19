42de2c49f23061b932fa3765c1552fb53beff13e
chris023 <chris.meyer023@gmail.com>
2021-10-18 22:48:31 -0600

    Add eslint configuration

---
+++ backend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['airbnb-base'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 13,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint, simple-import-sort'],
+    rules: {
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error',
+    },
+}
